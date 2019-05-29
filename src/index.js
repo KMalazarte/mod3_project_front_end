06bb7bda858678b47c6c98767e6b19194765ef39
David <davidbernier91@gmail.com>
2019-05-29 09:08:36 -0400

    css/refactoredJs

--- src/index.js
+++ src/index.js
@@ -1,58 +1,40 @@
+const bodyPartsURL = 'http://localhost:3000/api/v1/body_groups';
+const exercisesURL = 'http://localhost:3000/exercises'
+const bodyPartList = document.querySelector('#body_groups-list')
+const muscleCardSubtitle = document.querySelector('#muscleCardSubtitle')
+const pictureOrVideo = document.querySelector('#muscle-diagram')
 
 
+fetch(bodyPartsURL)
+  .then(res => res.json())
+  .then(data => {
 
-document.addEventListener('DOMContentLoaded', () => {
-  const endPoint = 'http://localhost:3000/api/v1/body_groups';
-  const exercisesURL = 'http://localhost:3000/exercises'
-  let bodygroupContainer = document.querySelector('#body_groups-container')
-  const exerciseShow = document.querySelector('#show')
-  // console.log(exerciseShow);
-  fetch(endPoint)
-    .then(res => res.json())
-    .then(body_groups => {
+    data.forEach(bodypart => {
+      bodyPartList.innerHTML += `
+        <div class="w3-ul w3-border">
+          <ul data-name=${bodypart.name}>${bodypart.name}
+          <button id='${bodypart.id}' class='button'>See Exercises</button>
+          </ul>
+        </ul>
+      `
+    })
+  })
 
-      // console.log(body_groups)
-      body_groups.forEach(bodypart => {
-        // console.log(bodypart)
-        const markup = `
-        <li>
-        <h3>${bodypart.name}
-        <button id = '${bodypart.id}'>See Exercises</button>
-        </h3>
-        </li>`;
-        const bodylist = document.querySelector('#body_groups-list').innerHTML += markup;
-      })//end of forEach
-    })//end first fetch
+bodyPartList.addEventListener('click', (e) => {
+  if (e.target.className === 'button') {
+    fetch(exercisesURL)
+      .then(res => res.json())
+      .then(exercises => {
+        const filterExercise = exercises.filter((exercise) => parseInt(e.target.id) === exercise.body_group_id)
 
-      bodygroupContainer.addEventListener('click', function (e) {
-        console.log("this is x", e.clientX)
-        console.log("this is y", e.clientY)
-        fetch(exercisesURL, {method: "GET"})
-        .then (resp => {return resp.json()})
-        .then (exercises => {
-          // exercises.forEach(exercise => {
-            // console.log(typeof(exercise.body_group_id))
-            const filterExercise = exercises.filter( exercise => parseInt(e.target.id) === exercise.body_group_id)
-            // console.log(filterExercise)
-            exerciseShow.innerHTML = ''
-            filterExercise.forEach( exercise => {
-              // console.log(exercise)
+        muscleCardSubtitle.innerHTML = ''
+        filterExercise.forEach(exercise => {
 
-              // const exerciseTitle = document.querySelector('#exercise-name')
-              // const exerciseContent = document.querySelector('#exercise-description')
-              // exerciseTitle.innerText = `${exercise.name}`
-              // debugger
-              exerciseShow.innerHTML +=
-              `<h3> ${exercise.name}</h3>
-              <p> ${exercise.description}</p> </br>
-              `
-           })//end of filterExercise forEach
-           // if(e.target.id === exercises.body_group_id)
-        })//exercises.forEach(exercise =>
-        })
-
-      // })//bodygroupContainer.addEventListener
-
-
-
-});// DOMContentLoaded
+          muscleCardSubtitle.innerHTML += `
+                      <h3> ${exercise.name}</h3>
+                      <p> ${exercise.description}</p> </br>
+                    `
+        }) //end of filterExercise forEach
+      }) //exercises.forEach(exercise =>
+  }
+})
